d435089a44e6c274e93efbf90001f17bcef7a9aa
wsw9603 <wsw9603@qq.com>
2020-10-21 22:25:18 +0800

    重命名globalmem为globalfifo
    Signed-off-by: wsw9603 <wsw9603@qq.com>

--- cdev_module.c
+++ cdev_module.c
@@ -7,7 +7,7 @@
 
 #include "cdev_module.h"
 
-#define DEVNAME "globalmem"
+#define DEVNAME "globalfifo"
 #define GLOBALMEM_SIZE 0x1000
 
 #define wws_pr_info(fmt, ...)	\
@@ -20,28 +20,28 @@ static int minor = 1;
 static int count = 3; /* 驱动所创建的设备数目 */
 module_param(count, int, S_IRUGO);
 
-struct globalmem_dev {
+struct globalfifo_dev {
 	struct cdev cdev;
 	char buf[GLOBALMEM_SIZE];
 	struct mutex mutex;
 };
 
 static struct class *pclass;
-static struct globalmem_dev *pglobalmem_dev;
+static struct globalfifo_dev *pglobalfifo_dev;
 
-static int globalmem_open(struct inode *inode, struct file *filp)
+static int globalfifo_open(struct inode *inode, struct file *filp)
 {
 	wws_pr_info("file opened by, (%s: pid = %d)",
 		    current->comm, current->pid);
 	wws_pr_info("major: %d, minor: %d", imajor(inode), iminor(inode));
 
 	filp->private_data = container_of(inode->i_cdev,
-					  struct globalmem_dev, cdev);
+					  struct globalfifo_dev, cdev);
 
 	return 0;
 }
 
-static int globalmem_release(struct inode *inode, struct file *filp)
+static int globalfifo_release(struct inode *inode, struct file *filp)
 {
 	wws_pr_info("file release by, (%s: pid = %d)",
 		    current->comm, current->pid);
@@ -50,10 +50,10 @@ static int globalmem_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static ssize_t globalmem_read(struct file *filp, char __user *buf,
+static ssize_t globalfifo_read(struct file *filp, char __user *buf,
 			      size_t size, loff_t *offset)
 {
-	struct globalmem_dev *pdev = filp->private_data;
+	struct globalfifo_dev *pdev = filp->private_data;
 	struct inode *inode = filp->f_path.dentry->d_inode;
 
 	wws_pr_info("file read by, (%s: pid = %d)",
@@ -78,10 +78,10 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf,
 	return size;
 }
 
-static ssize_t globalmem_write(struct file *filp, const char __user *buf,
+static ssize_t globalfifo_write(struct file *filp, const char __user *buf,
 			  size_t size, loff_t *offset)
 {
-	struct globalmem_dev *pdev = filp->private_data;
+	struct globalfifo_dev *pdev = filp->private_data;
 	struct inode *inode = filp->f_path.dentry->d_inode;
 
 	wws_pr_info("file write by, (%s: pid = %d)",
@@ -105,7 +105,7 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf,
 	return size;
 }
 
-static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
+static loff_t globalfifo_llseek(struct file *filp, loff_t offset, int orig)
 {
 	switch (orig) {
 	case SEEK_SET:
@@ -124,10 +124,10 @@ static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 	}
 }
 
-static long globalmem_ioctl(struct file *filp, unsigned int cmd,
+static long globalfifo_ioctl(struct file *filp, unsigned int cmd,
 			   unsigned long arg)
 {
-	struct globalmem_dev *pdev = filp->private_data;
+	struct globalfifo_dev *pdev = filp->private_data;
 
 	switch (cmd) {
 	case MEM_CLEAR:
@@ -144,20 +144,20 @@ static long globalmem_ioctl(struct file *filp, unsigned int cmd,
 
 static struct file_operations fops = {
 	.owner		= THIS_MODULE,
-	.open		= globalmem_open,
-	.release	= globalmem_release,
-	.read		= globalmem_read,
-	.write		= globalmem_write,
-	.llseek		= globalmem_llseek,
-	.unlocked_ioctl	= globalmem_ioctl,
+	.open		= globalfifo_open,
+	.release	= globalfifo_release,
+	.read		= globalfifo_read,
+	.write		= globalfifo_write,
+	.llseek		= globalfifo_llseek,
+	.unlocked_ioctl	= globalfifo_ioctl,
 };
 
-static int __init globalmem_setup(int idx)
+static int __init globalfifo_setup(int idx)
 {
 	int ret;
 	struct device *pdevice;
 	dev_t devnum = MKDEV(major, minor + idx);
-	struct globalmem_dev *pdev = pglobalmem_dev + idx;
+	struct globalfifo_dev *pdev = pglobalfifo_dev + idx;
 
 	cdev_init(&pdev->cdev, &fops);
 
@@ -184,16 +184,16 @@ error_out:
 	return ret;
 }
 
-static void globalmem_reset(int idx)
+static void globalfifo_reset(int idx)
 {
-	struct globalmem_dev *pdev = pglobalmem_dev + idx;
+	struct globalfifo_dev *pdev = pglobalfifo_dev + idx;
 
 	device_destroy(pclass, MKDEV(major, minor + idx));
 	cdev_del(&pdev->cdev);
 	mutex_destroy(&pdev->mutex);
 }
 
-static int __init globalmem_init(void)
+static int __init globalfifo_init(void)
 {
 	dev_t devnum = MKDEV(major, minor);
 	int ret, i;
@@ -214,8 +214,8 @@ static int __init globalmem_init(void)
 	}
 	major = MAJOR(devnum);
 
-	pglobalmem_dev = kzalloc(sizeof(*pglobalmem_dev) * count, GFP_KERNEL);
-	if (!pglobalmem_dev) {
+	pglobalfifo_dev = kzalloc(sizeof(*pglobalfifo_dev) * count, GFP_KERNEL);
+	if (!pglobalfifo_dev) {
 		ret = -ENOMEM;
 		goto error_reg;
 	}
@@ -228,7 +228,7 @@ static int __init globalmem_init(void)
 	}
 
 	for (i = 0; i < count; i++) {
-		ret = globalmem_setup(i);
+		ret = globalfifo_setup(i);
 		if (ret < 0)
 			goto error_setup;
 	}
@@ -237,31 +237,31 @@ static int __init globalmem_init(void)
 
 error_setup:
 	for (i--; i >= 0; i--)
-		globalmem_reset(i);
+		globalfifo_reset(i);
 	class_destroy(pclass);
 error_alloc:
-	cdev_del(&pglobalmem_dev->cdev);
+	cdev_del(&pglobalfifo_dev->cdev);
 error_reg:
 	unregister_chrdev_region(devnum, count);
 
 	return ret;
 }
 
-static void __exit globalmem_exit(void)
+static void __exit globalfifo_exit(void)
 {
 	int i;
 
 	wws_pr_info("exit demo: (%s: pid=%d)", current->comm, current->pid);
 
 	for (i = 0; i < count; i++)
-		globalmem_reset(i);
+		globalfifo_reset(i);
 	class_destroy(pclass);
 
 	unregister_chrdev_region(MKDEV(major, minor), count);
-	kfree(pglobalmem_dev);
+	kfree(pglobalfifo_dev);
 }
 
-module_init(globalmem_init);
-module_exit(globalmem_exit);
+module_init(globalfifo_init);
+module_exit(globalfifo_exit);
 
 MODULE_LICENSE("GPL v2");
