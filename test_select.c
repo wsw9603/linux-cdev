12f7200d0c1af6acd5c3d8cefcc1859fa702b264
wsw9603 <wsw9603@qq.com>
2020-11-01 14:34:43 +0800

    用select监听多个fd
    Signed-off-by: wsw9603 <wsw9603@qq.com>

--- test_select.c
+++ test_select.c
@@ -7,39 +7,65 @@
 
 #include "cdev_module.h"
 
+#define READ_BUF_LENGTH 100
+#define MAX_NAME_LENGTH 100
+#define FILES_TO_MONITOR 3
+#define FILE_BASENAME "/dev/globalfifo"
+
 int main(int argc, const char *argv[])
 {
-	int fd;
+	int max_fd = 0, fd;
+	int fds[FILES_TO_MONITOR];
 	fd_set rfds, wfds;
-
-	if (argc != 2) {
-		printf("Usage: %s filename\n", argv[0]);
-		return -1;
-	}
-
-	fd = open(fname, O_RDONLY | O_NONBLOCK);
-	if (fd < 0) {
-		printf("open file failed\n");
-		return fd;
-	}
-
-	if (ioctl(fd, MEM_CLEAR) < 0) {
-		printf("ioctl failed\n");
-		return -1;
-	}
+	char name_buf[MAX_NAME_LENGTH];
 
 	FD_ZERO(&rfds);
 	FD_ZERO(&wfds);
-	FD_SET(fd, &rfds);
-	FD_SET(fd, &wfds);
+	for (int i = 0; i < FILES_TO_MONITOR; i++) {
+		sprintf(name_buf, "%s%d", FILE_BASENAME, i + 1);
+		fd = open(name_buf, O_RDONLY | O_NONBLOCK);
+		if (fd < 0) {
+			printf("open file '%s' failed, %d\n", name_buf, fd);
+			return fd;
+		}
+
+		if (ioctl(fd, MEM_CLEAR) < 0) {
+			printf("ioctl failed\n");
+			return -1;
+		}
+
+		max_fd = max_fd > fd ? max_fd : fd;
+		FD_SET(fd, &rfds);
+		FD_SET(fd, &wfds);
+		fds[i] = fd;
+	}
 
 	for (int i = 0; i < 50; i++) {
-		int ret = select(fd + 1, &rfds, &wfds, NULL, NULL);
+		int length;
+		char read_buf[READ_BUF_LENGTH];
+		int ret = select(max_fd + 1, &rfds, &wfds, NULL, NULL);
 		printf("select result: %d\n", ret);
-		if (FD_ISSET(fd, &rfds))
-			printf("Poll monitor: ready to be read\n");
-		if (FD_ISSET(fd, &wfds))
-			printf("Poll monitor: ready to be written\n");
+
+		for (int j = 0; j < FILES_TO_MONITOR; j++) {
+			fd = fds[j];
+
+			if (FD_ISSET(fd, &rfds)) {
+				printf("file%d ready to be read\n", j + 1);
+				length = read(fd, read_buf, READ_BUF_LENGTH);
+				if (length < 0)
+					printf("read file failed, %d\n", length);
+				else
+					printf("read reault: %s, length: %d\n",
+							read_buf, length);
+			} else
+				FD_SET(fd, &rfds); /* 需要重新设置fd */
+
+			if (FD_ISSET(fd, &wfds))
+				printf("file%d ready to be written\n", j + 1);
+			else
+				FD_SET(fd, &wfds);
+		}
+
 		sleep(1);
 	}
 
